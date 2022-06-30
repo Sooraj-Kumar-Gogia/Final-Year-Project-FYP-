21f8ab92b51090e8af7de4fbc7dbcb060c0ad91b
Sooraj-Kumar-Gogia <surajgogia074@gmail.com>
2022-06-30 13:49:10 +0500

    working on backend

--- myproject/Backend/index.js
+++ myproject/Backend/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const {mongoUrl} = require('./keys')
+const mongoose = require('mongoose')
 const app = express()
 PORT = 3000
 
+mongoose.connect(mongoUrl)
+
+mongoose.connection.on('connected' , ()=>{
+    console.log('Yeaahh!! Connected to MongoDB')
+})
+
+mongoose.connection.on('eror' , (err)=>{
+    console.log('Opps! Could not connect', err)
+})
+
+
+
 app.get('/', (req, res)=>{
-    res.send('Hello')
+    res.send("Hello")
 })
 
 app.listen(PORT, ()=>{
